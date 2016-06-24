f48d77ee6f6a5ba9ac4ea8f7b9d0cce977286f3a
graham <gjm1324@vt.edu>
2016-06-24 11:51:09 -0400

    global php linux compatitble

--- app/global.php
+++ app/global.php
@@ -1,10 +1,20 @@
 <?php
-if(file_exists(__dir__."\config.php"))
-{
-	include_once __dir__."\config.php";
-	return true;
+if (PHP_OS === 'Linux'||PHP_OS === 'Unix') {
+	if(file_exists(__dir__."/config.php"))
+	{
+		include_once __dir__."/config.php";
+		return true;
+	}
+	else{echo 'Couldn\'t find the config file';}
 }
-else{echo 'Couldnt find the config file';}
+else {
+	if(file_exists(__dir__."\config.php"))
+	{
+		include_once __dir__."\config.php";
+		return true;
+	}
+	else{echo 'Couldn\'t find the config file';}
+}
 
 function __autoload($class_name){
 	require_once 'model/'.$class_name.'.class.php';
